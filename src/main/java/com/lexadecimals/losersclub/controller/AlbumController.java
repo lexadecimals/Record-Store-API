ef48e0aad0752c6ab0adac66fe885e267711d7ee
lexadecimals <alexis.wilkinsondefoe@gmail.com>
2024-12-14 10:55:25 +0000

    feat: getallalbums implementation

--- src/main/java/com/lexadecimals/losersclub/controller/AlbumController.java
+++ src/main/java/com/lexadecimals/losersclub/controller/AlbumController.java
@@ -1,14 +1,28 @@
 package com.lexadecimals.losersclub.controller;
 
+import com.lexadecimals.losersclub.model.Album;
 import com.lexadecimals.losersclub.service.AlbumServiceImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+import java.util.List;
+
 @RestController
+@RequestMapping("/albums")
 public class AlbumController {
 
     @Autowired
     AlbumServiceImpl albumServiceImpl;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Album>> getAllAlbums() {
+        // TODO: customise response for empty list
+        Iterable<Album> allAlbums = albumServiceImpl.getAllAlbums();
+        return new ResponseEntity<>(allAlbums, HttpStatus.OK);
+    }
 }
--- src/main/java/com/lexadecimals/losersclub/service/AlbumServiceImpl.java
+++ src/main/java/com/lexadecimals/losersclub/service/AlbumServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lexadecimals.losersclub.service;
 
+import com.lexadecimals.losersclub.model.Album;
 import com.lexadecimals.losersclub.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class AlbumServiceImpl implements AlbumService {
 
     @Autowired
     AlbumRepository albumRepository;
+
+    public Iterable<Album> getAllAlbums() {
+        return albumRepository.findAll();
+    }
 }
