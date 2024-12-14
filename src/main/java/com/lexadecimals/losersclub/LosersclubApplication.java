bc14cd85308d8d8839881b179a6b5efd0ea6ceb4
lexadecimals <alexis.wilkinsondefoe@gmail.com>
2024-12-14 10:23:57 +0000

    first commit :tada:

---
+++ src/main/java/com/lexadecimals/losersclub/LosersclubApplication.java
@@ -0,0 +1,13 @@
+package com.lexadecimals.losersclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosersclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosersclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexadecimals/losersclub/LosersclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexadecimals.losersclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LosersclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
