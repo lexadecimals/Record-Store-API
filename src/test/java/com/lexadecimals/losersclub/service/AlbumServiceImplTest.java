71f4b23d30b150e9731c441e4d7b9cbda2ae9f91
lexadecimals <alexis.wilkinsondefoe@gmail.com>
2025-01-04 19:15:02 +0000

    test: remove put test

--- src/test/java/com/lexadecimals/losersclub/service/AlbumServiceImplTest.java
+++ src/test/java/com/lexadecimals/losersclub/service/AlbumServiceImplTest.java
@@ -2,6 +2,7 @@ package com.lexadecimals.losersclub.service;
 
 import com.lexadecimals.losersclub.model.Album;
 import com.lexadecimals.losersclub.repository.AlbumRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -65,23 +66,7 @@ class AlbumServiceImplTest {
         Album result = albumServiceImpl.addAlbum(album);
         assert(result.equals(album));
     }
-
-//    @Test
-//    @DisplayName("PUT /{id} returns updated Album")
-//    void updateAlbumTest() {
-//        Album existingAlbum = albums.getLast();
-//        Album albumUpdates = albums.get(1);
-//        albumUpdates.setTitle("Five Leaves Left");
-//        albumUpdates.setYearOfRelease(1969);
-//        when(albumRepositoryMock.save(existingAlbum)).thenReturn(existingAlbum);
-//        when(albumRepositoryMock.save(albumUpdates)).thenReturn(albumUpdates);
-//        Album result = albumServiceImpl.updateAlbum(1L, albumUpdates);
-//        assertEquals(result.getId(), 1L);
-//    }
 }
-
-//method is working but test is not
-
 
 
 
